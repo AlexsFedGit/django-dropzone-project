3eda6ea784673bd7447b821c82b6224ffe339f6c
Alex Fed <AlexFedGit@Gmail.com>
2023-04-07 21:04:40 +0300

    Init basic models, urls

---
+++ home/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date']
+
+    def __str__(self):
+        return str(self.date)
---
+++ home/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
+from home.models import Image
+
+
+def home(request):
+    images = Image.objects.all()
+    context = {
+        'images': images
+    }
+    return render(request, 'index.html', context)
+
+
+def file_upload(request):
+    if request.method == 'POST':
+        my_file = request.FILES.get('file')
+        if my_file:
+            Image.objects.create(image=my_file)
+            return HttpResponse('')
+        else:
+            return HttpResponse(
+                '<h1>500 Internal Server Error (Внутренняя ошибка сервера)</h1>',
+                status=500)
+    return JsonResponse({'post': 'false'})
