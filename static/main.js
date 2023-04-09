aa0d5a2ada5fd37136910b9ed63952fe7eeb5a00
Alex Fed <AlexFedGit@Gmail.com>
2023-04-09 15:45:42 +0300

    Dropzone config error fixed

--- static/main.js
+++ static/main.js
@@ -2,8 +2,7 @@ Dropzone.autoDiscover=false;
 const myDropzone = new Dropzone('#my-dropzone', {
         url: 'upload/',
         maxFiles: 5,
-        maxFilesize: 2,
+        maxFilesize: 10,
         acceptedFiles: 'image/*',
         dictDefaultMessage: "Перетащите файлы для загрузки на сайт",
-        maxFilesize: 10,
 })
